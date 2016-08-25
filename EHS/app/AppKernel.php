049105efd637c058a3a64485610d262778a161fc
Vincent Léné <vincent.lene.dl@gmail.com>
2016-08-25 13:41:07 +0200

    Ajout du ForumBundle et du ArticleBundle

--- EHS/app/AppKernel.php
+++ EHS/app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new UserBundle\UserBundle(),
+            new ForumBundle\ForumBundle(),
+            new ArticleBundle\ArticleBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {

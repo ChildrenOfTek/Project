125933bbd9f867541f07748e03fcfae6c88e6189
jib0ums <jeanbb@gmail.com>
2016-09-02 13:00:09 +0200

    Modif des twig pour erreur date Article fonctionnel

---
+++ EHS/src/ArticleBundle/Form/ArticleType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ArticleBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('user')
+            ->add('dateArticle')
+            ->add('titreArticle')
+            ->add('sujetArticle')
+            ->add('content')
+            ->add('datePublication')
+            ->add('imageArticle')
+            ->add('online')
+            ->add('tag')
+            ->add('newsletter')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ArticleBundle\Entity\Article'
+        ));
+    }
+}

7be329227fe95de9443a214208a4747d7c91e043
Pilepamla <pierre.lepage.44@gmail.com>
2016-09-22 09:27:07 +0200

    ajout agenda bundles

--- EHS/app/AppKernel.php
+++ EHS/app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new ForumBundle\ForumBundle(),
             new ArticleBundle\ArticleBundle(),
             new NewsletterBundle\NewsletterBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {

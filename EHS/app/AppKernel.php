cdf86f262c95aa5184f996278f82d4572b755184
Pilepamla <pierre.lepage.44@gmail.com>
2016-09-22 11:40:36 +0200

    conflict resolution

--- EHS/app/AppKernel.php
+++ EHS/app/AppKernel.php
@@ -20,8 +20,6 @@ class AppKernel extends Kernel
             new ForumBundle\ForumBundle(),
             new ArticleBundle\ArticleBundle(),
             new NewsletterBundle\NewsletterBundle(),
-            // new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            // new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {

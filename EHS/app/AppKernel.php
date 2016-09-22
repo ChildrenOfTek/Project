c6b54af5d16a0d8886f63aeb6cde97048cb4429c
Pilepamla <pierre.lepage.44@gmail.com>
2016-09-22 10:48:14 +0200

    calendar bundle

--- EHS/app/AppKernel.php
+++ EHS/app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new AssociationBundle\AssociationBundle(),
             new UserBundle\UserBundle(),
             new ForumBundle\ForumBundle(),
             new ArticleBundle\ArticleBundle(),

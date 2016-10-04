fe55f76e5f0975d7b72ce543ecbc5f4a819ddc8c
Nico <nicolas.gicquel10@gmail.com>
2016-10-04 16:54:08 +0200

    generate SondageBundle+moifs fin de journée

---
+++ EHS/src/SondageBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SondageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('SondageBundle:Default:index.html.twig');
+    }
+}

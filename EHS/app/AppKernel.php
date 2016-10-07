e2d7a87f02fc0ac2dbb883536ee75c134b3fb793
jib0ums <jeanbb@gmail.com>
2016-10-07 10:11:34 +0200

    footer et insertion image dans le wysiwyg

--- EHS/app/AppKernel.php
+++ EHS/app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Vich\UploaderBundle\VichUploaderBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
+            new FM\ElfinderBundle\FMElfinderBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
